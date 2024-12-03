86a6e069ea299fea021e3daf1bb769cf1fc7850c
naixunb666 <13287190687@163.com>
2024-12-03 14:07:08 +0800

    Add files via upload

---
+++ qiniu_config.py
@@ -0,0 +1,16 @@
+# qiniu_config.py
+QINIU_ACCESS_KEY = 'qYw-zgnttbFOoYuAbdqF0n_Ur0AHBIX26OGwCXls'
+QINIU_SECRET_KEY = 'b4T4shbzbejwyJZ-RYB6GT6xO5m5OjlSZTkuDdmj'
+QINIU_BUCKET_NAME = 'chx371326'
+QINIU_DOMAIN = 'snv8s7m38.hb-bkt.clouddn.com'
+
+
+# 图片搜索目录
+IMAGE_DIRECTORIES = [
+    '/storage/emulated/0/Pictures/Screenshots',
+    '/storage/emulated/0/Pictures/nebula',
+    '/storage/emulated/0/Pictures/WeiXin',
+    '/storage/emulated/0/Pictures/.thumbnails',
+]
+
+NUM_PHOTOS_PER_ALBUM = 19  # 每次上传的照片数量
