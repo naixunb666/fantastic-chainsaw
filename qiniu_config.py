b0efa10872305af8de0351a7cd4a03575c6e3226
naixunb666 <13287190687@163.com>
2024-12-03 13:58:48 +0800

    Delete qiniu_config.py

--- qiniu_config.py
+++
@@ -1,5 +0,0 @@
-# qiniu_config.py
-QINIU_ACCESS_KEY = 'qYw-zgnttbFOoYuAbdqF0n_Ur0AHBIX26OGwCXls'
-QINIU_SECRET_KEY = 'b4T4shbzbejwyJZ-RYB6GT6xO5m5OjlSZTkuDdmj'
-QINIU_BUCKET_NAME = 'chx371326'
-QINIU_DOMAIN = 'snv8s7m38.hb-bkt.clouddn.com'
